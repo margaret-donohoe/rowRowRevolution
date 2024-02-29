7930b6b2fbcd9d873b2a09291876889da15bd5a1
luchaaa <lgortana@pratt.edu>
2024-02-29 11:54:30 -0500

    multiplayer animation

--- Assets/scripts/pInput2b.cs
+++ Assets/scripts/pInput2b.cs
@@ -38,6 +38,10 @@ public class pInput2b : MonoBehaviour
     public Sprite orchestraBoat;
     public Sprite toyBoat;  
 
+    public Animator animator;
+    private float old_ypos;
+    private float new_ypos;
+
     public float r1 = 0.25f;
     public float r2 = 0.75f;
     public float r3 = 1;
@@ -105,12 +109,14 @@ public class pInput2b : MonoBehaviour
     void assignMusicBoat() {
         if (boatType == "fancy") {
             gameObject.GetComponent<SpriteRenderer>().sprite = fancyBoat;
+            animator.SetBool("boattypeFancy", true);
             musicSource.clip = fancyMusic;
             musicSource.Play();
         }
 
         if (boatType == "orchestra") {
             gameObject.GetComponent<SpriteRenderer>().sprite = orchestraBoat;
+            animator.SetBool("boattypeOrchestra", true);
             musicSource.clip = orchestraMusic;
             musicSource.Play();
         }
@@ -166,8 +172,17 @@ public class pInput2b : MonoBehaviour
 
             if (CurrentNode < PathNode.Length - 1)
             {
+                old_ypos = CurrentPositionHolder.y;
                 CurrentNode++;
                 CheckNode();
+                new_ypos = CurrentPositionHolder.y;
+
+                if (old_ypos > new_ypos) {
+                    animator.SetBool("goingUp", false);
+                }
+                else if (old_ypos < new_ypos) {
+                    animator.SetBool("goingUp", true);
+                }
             }
         }
     }
