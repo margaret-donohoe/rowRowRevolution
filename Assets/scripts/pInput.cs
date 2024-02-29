de560c0c8dae3a8c7d37369142e612fe4f691a75
luchaaa <lgortana@pratt.edu>
2024-02-29 11:26:26 -0500

    Revert "IT WORKS!!!"
    This reverts commit 8e412d3e5fae035a72a56efadef37f93315a91c9.

--- Assets/scripts/pInput.cs
+++ Assets/scripts/pInput.cs
@@ -39,8 +39,6 @@ public class pInput : MonoBehaviour
     public Sprite orchestraBoat;
     public Sprite toyBoat;  
     public Animator animator;
-    private float old_ypos;
-    private float new_ypos;
 
     public float r1 = 0.25f;
     public float r2 = 0.75f;
@@ -176,22 +174,10 @@ public class pInput : MonoBehaviour
         {
             music.CheckTime();
 
-            
-
             if (CurrentNode < PathNode.Length - 1)
             {
-                old_ypos = CurrentPositionHolder.y;
                 CurrentNode++;
                 CheckNode();
-                new_ypos = CurrentPositionHolder.y;
-                print(old_ypos);
-                print(new_ypos);
-                if (old_ypos > new_ypos) {
-                    animator.SetBool("goingUp", false);
-                }
-                else if (old_ypos < new_ypos) {
-                    animator.SetBool("goingUp", true);
-                }
             }
         }
     }
